c4e3c2314c3082dad0d652b0aefdc01dde0a8805
kyusupov33 <kirill.yusupov33@gmail.com>
2018-10-26 11:36:13 +0300

    feat: add factory exception

--- src/utils/exception.ts
+++ src/utils/exception.ts
@@ -1,7 +1,14 @@
 import { SchematicsException } from '@angular-devkit/schematics';
 
-export function factoryException<T>(options: T | any) {
-    if (!options.name) {
-        throw new SchematicsException(`[WARN!] --name option is required.`);
-    }
+type factoryOptions<T> = T | any;
+
+export function factoryException<T>(options: factoryOptions<T>) {
+    const properties = Object.keys(options);
+    console.log(properties);
+    properties.forEach((prop: factoryOptions<T>) => {
+        if (prop.name && !prop.name.value) {
+            throw new SchematicsException(`[WARN!] --name option is required.`);
+        }
+        return prop;
+    });
 }
