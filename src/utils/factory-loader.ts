f102638c292b7be5b482c8f046e6473f6968f84a
eranshmil <eranshmil@users.noreply.github.com>
2018-12-24 23:53:46 +0200

    refactor(factory): remove unsued factory exception
    The if inside factoryException is never used, the prop is only a string that represents the property in the options.
    Also, this code is applied to all the schematics, while only 3 of them have the name property (store, state & actions).

--- src/utils/factory-loader.ts
+++ src/utils/factory-loader.ts
@@ -3,10 +3,8 @@ import { StarterKitSchema } from '../factories/starter-kit/starter-kit.schema';
 import { mergeWith, Rule } from '@angular-devkit/schematics';
 import { generate } from './generate-factory';
 import { FACTORIES } from './common/factories.enum';
-import { factoryException } from './exception';
 
 export function factoryLoader<T>(options: T | any, factory: FACTORIES): Rule {
-  factoryException<T>(options);
   options = transform<StarterKitSchema>(options);
   return mergeWith(generate({ options, factory }));
 }
